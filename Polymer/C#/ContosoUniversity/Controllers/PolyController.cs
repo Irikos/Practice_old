d3c63ed9a6cee725e81bd6f8d9f888ce77363b4d
Irikos <andrei.bardezi@gmail.com>
2016-08-18 19:48:46 +0300

    Polycasts & updates

---
+++ Polymer/C#/ContosoUniversity/Controllers/PolyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using System.Data.Entity.Infrastructure;
+
+namespace ContosoUniversity.Controllers
+
+{
+    public class PolyController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: Poly
+        public ActionResult Index()
+        {
+           Course c = db.Courses.Find(2);
+            
+            return View(c);
+        }
+        public String Result()
+        {
+            Course c = db.Courses.Find(2);
+
+            return "{\"SortAs\": \"SGML\"}";
+        }
+    }
+}
